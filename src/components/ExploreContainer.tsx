0748aa9da4ba5f6f2985d08354d07cba7c983683
tandemup <ustaticdb@gmail.com>
2023-01-28 15:24:53 +0100

    third commit

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -5,7 +5,7 @@ interface ContainerProps { }
 const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <div className="container">
-      <strong>Ready tooo create an app?</strong>
+      <strong>Ready toodo create an app?</strong>
       <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
     </div>
   );
